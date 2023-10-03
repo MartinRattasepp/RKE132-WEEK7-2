0adb9d7fd52472cf1b0f3dd7e6eb7f42aec2dd7b
Martin R <martin.rattasepp@tktk.ee>
2023-10-03 19:11:18 +0300

    add week7 code

---
+++ Week7-2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Are you coming or leaving? (in/out):");
+string userChoice = Console.ReadLine();
+
+if (userChoice == "in")
+{
+    Printhello();
+}
+else
+{
+    printGoodBye();
+}
+
+static void Printhello() //function / method
+{
+    Console.WriteLine("Hello, world!");
+}
+
+static void printGoodBye()
+{
+    Console.WriteLine("See you later, aligator.");
+}
